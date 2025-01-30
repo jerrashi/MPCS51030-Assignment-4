afaa350ddb86f8f9dfa293b1559b35e8d64a1da0
Jerry Shi <40506087+jerrashi@users.noreply.github.com>
2025-01-30 15:08:47 -0600

    Version 0.0
    - set up project
    - created new view for grid
    - see readme for more details

---
+++ Assignment 4/Assignment 4/GridView.swift
@@ -0,0 +1,20 @@
+//
+//  GridView.swift
+//  Assignment 4
+//
+//  Created by Jerry Shi on 1/30/25.
+//
+
+import UIKit
+
+class GridView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Assignment 4/Assignment 4/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Assignment 4
+//
+//  Created by Jerry Shi on 1/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Draw a grid using UIBezierPath
+        // iphone 15 pro size: 852 x 393
+        let screenHeight = self.view.frame.height
+        let screenWidth = self.view.frame.width
+        
+        
+    }
+}

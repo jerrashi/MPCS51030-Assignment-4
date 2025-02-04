f659bbe365228e7897818206b0722c3c8538fd22
Jerry Shi <40506087+jerrashi@users.noreply.github.com>
2025-02-03 21:31:04 -0600

    version 0.8
    - added snap to grid animations
    - see readme for more details

--- Assignment 4/Assignment 4/ViewController.swift
+++ Assignment 4/Assignment 4/ViewController.swift
@@ -10,45 +10,51 @@ import UIKit
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
     // Set Up Outlets
-    @IBOutlet var squares: [UIView]!
-    // MARK: Do we need this outlet?
-    @IBOutlet weak var infoButton: UIButton!
+    @IBOutlet var squares: [UILabel]!
     @IBOutlet weak var infoView: InfoView!
     @IBOutlet weak var infoViewLabel: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var oLabel: UILabel!
+    @IBOutlet weak var customGridView: CustomGridView!
     
+    var isXTurn = false
+    var initialCenter: CGPoint = .zero
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // Double check that grid is see through besides drawn lines
+        customGridView.backgroundColor = .clear
+        customGridView.isOpaque = false
+        
         /*
-        // Get screen dimensions
-        // iphone 15 pro size: 852 x 393
+        // screen dimensions
+        // iphone 15 pro: 852 x 393
         let screenHeight = self.view.frame.height
         let screenWidth = self.view.frame.width
          */
         
-        // Keep infoView off screen to begin with
+        // Make sure infoView is off screen to begin with
         infoView.frame.origin.y = -infoView.frame.height
         //infoView.center.x = self.view.frame.width / 2
         
-        // TO DO: - Optional (create views in storyboard and add outlet to view controller)
-        // Create "X" and "O" UILabel Views
-        let xLabel = UILabel(frame: CGRect(x: 16, y: 645, width: 125, height: 125))
+        // Modify "X" and "O" UILabel Views
+        // let xLabel = UILabel(frame: CGRect(x: 16, y: 645, width: 125, height: 125))
         xLabel.text = "X"
         xLabel.textAlignment = .center
         xLabel.font = UIFont.systemFont(ofSize: 120, weight: .bold)
         xLabel.textColor = .white
         xLabel.backgroundColor = .blue
-        xLabel.isUserInteractionEnabled = true
+        xLabel.isUserInteractionEnabled = false
         
-        let oLabel = UILabel(frame: CGRect(x: 252, y: 645, width: 125, height: 125))
+        // let oLabel = UILabel(frame: CGRect(x: 252, y: 645, width: 125, height: 125))
         oLabel.text = "O"
         oLabel.textAlignment = .center
         oLabel.font = UIFont.systemFont(ofSize: 120, weight: .bold)
         oLabel.textColor = .white
         oLabel.backgroundColor = .red
-        oLabel.isUserInteractionEnabled = true
+        oLabel.isUserInteractionEnabled = false
         
         // Add Pan Gesture Recognizer to X Label
         let xPanGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
@@ -64,6 +70,9 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         self.view.addSubview(xLabel)
         self.view.addSubview(oLabel)
         
+        // Set up x as first turn
+        switchTurn()
+        
         /// OPTIONAL: Enable tap gesture on infoView to dismiss alert screen
         /*
         // Add Tap Gesture Recognizer to infoView
@@ -76,12 +85,74 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     // MARK: - Gesture Metods
     @objc func handlePan(_ gestureRecognizer: UIPanGestureRecognizer){
-        let translation = gestureRecognizer.translation(in: self.view)
-        
         if let view = gestureRecognizer.view{
-            view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
-            gestureRecognizer.setTranslation(CGPoint.zero, in: self.view)
+            switch gestureRecognizer.state{
+            case .began:
+                // TO-DO: Change view to snap to center of touch
+                initialCenter = view.center
+            case .changed:
+                let translation = gestureRecognizer.translation(in: self.view)
+                view.center = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+                // Note: We don't use this since we track from starting position, instead of cumulative movement
+                //gestureRecognizer.setTranslation(CGPoint.zero, in: self.view)
+            case .ended, .cancelled:
+                // TO-DO: Implement logic to snap views
+                for square in squares{
+                    if square.frame.contains(view.center) /*&& grid.isEmpty(square: square.tag)*/ {
+                        // Animate piece "snapping" into place
+                        UIView.animate(withDuration: 1, animations: {
+                            // Move view from off screen to middle of screen vertically
+                            view.center = square.center
+                        })
+                        square.backgroundColor = isXTurn ? .blue : .red
+                        square.textAlignment = .center
+                        square.text = isXTurn ? "X" : "O"
+                        square.font = UIFont.systemFont(ofSize: 120, weight: .bold)
+                        square.textColor = UIColor.white
+                        
+                        // Update grid model
+                        // grid[tag] = isXTurn ? "X" : "O"
+                        
+                        // Hide view so snap back to spot isn't visible
+                        view.isHidden = true
+                        
+                        // Check for winner
+                        //checkWinner()
+                        
+                        switchTurn()
+                        }
+                }
+                
+                // Animate piece snapping back to starting spot
+                UIView.animate(withDuration: 0.25, animations: {
+                    // Move view from off screen to middle of screen vertically
+                    view.frame.origin.y = 645
+                    // Unwrap view as UILabel
+                    if let label = view as? UILabel {
+                        if label.text == "X" {
+                            label.frame.origin.x = 16
+                        } else {
+                            label.frame.origin.x = 252
+                        }
+                    }
+                    else {
+                        print("ERROR: View is NOT a Label")
+                    }
+                })
+                
+                // Make sure piece is no longer hidden
+                view.isHidden = false
+                
+            default:
+                break
+            }
         }
+        else {
+            print("ERROR - gesture recognizer view not unwrapped")
+        }
+        
+        
+        
         
     }
     
@@ -101,9 +172,13 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     */
     
     // MARK: - Button Functions
+    // Info button to bring infoView to center of screen
     @IBAction func infoButtonTapped(_ sender: UIButton) {
         // Update infoViewLabel text
         infoViewLabel.text = "Get 3 in a row to win!"
+        
+        // Make sure infoView is on top of stack
+        self.view.bringSubviewToFront(infoView)
                 
         // Animate infoView
         UIView.animate(withDuration: 1, delay: 0, options: .curveEaseIn, animations: {
@@ -112,7 +187,11 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         })
     }
     
+    // OK button to move infoView to below screen
     @IBAction func infoViewButtonTapped(_ sender: UIButton) {
+        // Make sure infoView is on top of stack
+        self.view.bringSubviewToFront(infoView)
+        
         // Animate infoView
         UIView.animate(withDuration: 1, delay: 0, options: .curveEaseIn, animations: {
             // Move view from off screen to middle of screen vertically
@@ -124,5 +203,27 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         }
     }
     
+    // MARK: -Gameplay Functions
+    func switchTurn() {
+        // Store current piece
+        let oldPiece = isXTurn ? xLabel : oLabel
+        let newPiece = isXTurn ? oLabel : xLabel
+        
+        // Switch isXTurn value
+        isXTurn.toggle()
+
+        // Disable old piece
+        oldPiece?.alpha = 0.25
+        oldPiece?.isUserInteractionEnabled = false
+        
+        // Animate & Enable new piece
+        newPiece?.alpha = 1
+        UIView.animate(withDuration: 0.25, animations: {
+            newPiece?.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        }) {_ in UIView.animate(withDuration: 0.5) {
+            newPiece?.transform = CGAffineTransform.identity
+            newPiece?.isUserInteractionEnabled = true
+        }}
+    }
     
 }
--- README.md
+++ README.md
@@ -20,7 +20,20 @@ Features Checklist (In Progress):
 ✅ Create a UIView subclass called InfoView and add a label and button  
 ✅ Add rounded corners, a border color, and aborder width to the InfoView view by overriding awakeFromNib() and CALayer.  
 ✅ Add info button. When user taps, update infoView text to instructions and animate infoView to move from offscreen to center of screen.  
-✅ When user taps "OK" button in infoVoew, animate infoView to from center of screen to offscreen. Reset the infoView to above screen to reset for future animations.    
+✅ When user taps "OK" button in infoVoew, animate infoView to from center of screen to offscreen. Reset the infoView to above screen to reset for future animations.  
+✅ "X" piece starts the game.  
+✅ Set piece whose turn it is to alpha of 1.  
+✅ Use an animation to show to user(s) whose turn it is. User interaction is disabled until animation is completed.  
+✅ When user drops piece on unoccupied tile, animate the piece "snapping" in place on the grid.  
+✅ When user drops piece on a location outside the grid, animate the piece "snapping" back to starting position on the grid.  
+✅ When a piece occupies a spot on the grid, make sure it is no longer user interactable (NOTE: In my implementation, I don't leave the game piece on the grid. Instead, I unhide the square view and set their text & appearance to match the piece that was placed there. Then, I hide the original game piece and move it back to its original spot. See my viewController for more details.)  
+
+
+
+
+
+  
+
 
 🌟 Bonus Features:  
 ❌ .  
@@ -30,3 +43,6 @@ Asynchronous Class Lecture Videos
 IBOutlet Collection Tutorial (https://www.youtube.com/watch?v=mr7pJB2eyK4)  
 UIView.Animate Tutortial (https://www.hackingwithswift.com/example-code/uikit/how-to-animate-views-using-animatewithduration)  
 UIViewProperty Animator Tutorial (https://www.hackingwithswift.com/example-code/uikit/how-to-animate-views-using-uiviewpropertyanimator)  
+Transform: CGAffine Transform Tutorial (https://www.hackingwithswift.com/read/15/4/transform-cgaffinetransform)  
+Bring Subview to Front Tutorial (https://www.hackingwithswift.com/example-code/uikit/how-to-bring-a-subview-to-the-front-of-a-uiview)  
+UIPanGestureRecognizer Tutorial w/ States (https://cocoacasts.com/swift-fundamentals-working-with-pan-gesture-recognizers-in-swift)  
